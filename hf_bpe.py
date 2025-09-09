e3dd8e6dbbe331030427e7a9d10e3cd234409907
Stephen <stephen@umw.edu>
2025-09-08 21:46:21 -0400

    Add demo code for HF's BPE tokenizer.

---
+++ hf_bpe.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+'''
+Demo code that uses HuggingFace's BPE tokenizer.
+
+DATA 470D3
+Stephen Davies, University of Mary Washington, fall 2025
+'''
+from tokenizers import ByteLevelBPETokenizer
+from pprint import pprint
+
+CORPUS = "bpe_corpus.txt"
+N_MERGES = 20
+
+tokenizer = ByteLevelBPETokenizer()
+
+tokenizer.train(
+    [CORPUS],                 # list of files to train on
+    vocab_size=256+N_MERGES,  # approximate; depends on initial alphabet
+)
+
+print("The vocab is:")
+vocab = sorted(tokenizer.get_vocab().items(), key=lambda x: x[1])
+pprint(vocab)
+
+tokenizer.save("tokenizer.json")
+
+# Tokenize ("encode") and de-tokenize ("decode")
+text = input("Enter text to encode (or done): ")
+while text != "done":
+    encoded = tokenizer.encode(text)
+    print(encoded.ids)       # token ids
+    print(encoded.tokens)    # token strings
+    print(tokenizer.decode(encoded.ids))  # back to text
+    text = input("Enter text to encode (or done): ")
